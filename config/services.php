e933caacf395d84977277386c2ee77ff68c8af68
Diego01GD <e22080693@itmerida.edu.mx>
2026-05-27 00:43:38 -0600

    Fix: Hacer cacheables las llaves VAPID en produccion

--- config/services.php
+++ config/services.php
@@ -41,4 +41,9 @@ return [
         'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],
 
+    'vapid' => [
+        'public_key' => env('VAPID_PUBLIC_KEY'),
+        'private_key' => env('VAPID_PRIVATE_KEY'),
+    ],
+
 ];
